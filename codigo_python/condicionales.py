18052d6251f15f15b7cf689ac9b05e765a886030
Cristian Correa <cristian.correa.cs@gmail.com>
2022-02-01 20:55:24 -0500

    modificación del conversor de moneda para agregar más paises

---
+++ codigo_python/condicionales.py
@@ -0,0 +1,19 @@
+# edad = int(input("Escribe tu edad"))
+
+# if edad > 17:
+#     # pass
+#     print("Es mayor a 17")
+# else:
+#     # pass
+#     print("No es mayor de 17")
+    
+
+
+numero = int(input("Escribe número: "))
+
+if numero > 5:
+    print("Es mayor a 5")
+elif numero == 5:
+    print("Es igual a 5")
+else:
+    print("Es menor a 5")
